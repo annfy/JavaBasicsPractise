37099fd79b1833d64554ec0e24c4fef296d8ca50
Jarvis <712369460@qq.com>
2022-08-01 20:26:21 +0800

    JavaGuide练习

--- src/main/java/com/guide/bascis/JavaBascis.java
+++ src/main/java/com/guide/bascis/JavaBascis.java
@@ -9,7 +9,64 @@ package com.guide.bascis;
 
 public class JavaBascis {
 
-    public static void main(String[] args) {
+
+      class Address implements Cloneable {
+
+        private String name;
+
+
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
+
+        @Override
+        public Address clone() {
+            try {
+                return (Address) super.clone();
+            } catch (CloneNotSupportedException e) {
+                throw new AssertionError();
+            }
+        }
+    }
+
+
+     class Person implements Cloneable{
+        private Address address;
+
+        public Address getAddress() {
+            return address;
+        }
+
+        public void setAddress(Address address) {
+            this.address = address;
+        }
+
+        @Override
+        public Person clone(){
+            try {
+                Person person=(Person) super.clone();
+                return person;
+            } catch (CloneNotSupportedException e) {
+                throw new AssertionError();
+            }
+        }
+
+
+
+    }
+
+
+
+    public  void main(String[] args) {
+        Person person=new Person();
+
+
+
+
 
     }
 
