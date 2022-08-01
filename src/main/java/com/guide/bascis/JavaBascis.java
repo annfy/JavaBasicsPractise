3c4adb18214190a99afd2ffa6d7fe8998223cb35
Jarvis <712369460@qq.com>
2022-08-01 20:13:06 +0800

    JavaGuide练习

---
+++ src/main/java/com/guide/bascis/JavaBascis.java
@@ -0,0 +1,17 @@
+package com.guide.bascis;
+
+/**
+ * @author Jarvis
+ * @version 1.0.1
+ * @description
+ * @date 2022-08-01 20:01
+ */
+
+public class JavaBascis {
+
+    public static void main(String[] args) {
+
+    }
+
+
+}
